b6dd1dabeace53526a1d3a6aa317720f3d8ee438
jbg3 <jbg3@sina.com>
2018-09-10 20:38:25 +0800

    列表查询完善

---
+++ network.js
@@ -0,0 +1,103 @@
+/**
+ * url:请求的url
+ * params:请求参数
+ * message:loading提示信息
+ * success:成功的回调
+ * fail:失败的回调
+ */
+
+//post请求
+function postRequest(url, params, success, fail) {
+     this.postRequestLoading(url, params, "", success, fail)
+}
+
+//根据判断message 是否显示loading
+function postRequestLoading(url, params, message, success, fail) {
+     if (message != "") {
+          wx.showLoading({
+               title: message,
+          })
+     }
+     const postRequestTask = wx.request({
+          url: url,
+          data: params,
+          header: {
+               'content-type': 'application/x-www-form-urlencoded',
+               'token':getApp().globalData.token
+          },
+          method: 'POST',
+          success: function(res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               if (res.statusCode == 200) {
+                    success(res.data)
+               } else {
+                    fail(res)
+               }
+          },
+          fail: function(res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               fail(res)
+          }
+     })
+}
+
+//get请求
+function getRequest(url, params, success, fail) {
+     this.getRequestLoading(url, params, "", success, fail)
+}
+
+function getRequestLoading(url, params, message, success, fail) {
+     if (message != "") {
+          wx.showLoading({
+               title: message,
+          })
+     }
+     const getRequestTask = wx.request({
+          url: url,
+          data: params,
+          header: {
+               'Content-Type': 'application/json',
+               'token':getApp().globalData.token
+          },
+          method: 'GET',
+          success: function(res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               if (res.statusCode == 200) {
+                    success(res.data)
+               } else {
+                    fail(res)
+               }
+          },
+          fail: function(res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               fail(res)
+          }
+     })
+}
+
+//取消post请求
+function abortPostRequest(url, params, success, fail) {
+     postRequestTask.abort()
+}
+
+//取消get请求
+function abortGetRequest(url, params, success, fail) {
+     getRequestTask.abort()
+}
+
+module.exports = {
+     postRequest: postRequest,
+     postRequestLoading: postRequestLoading,
+     getRequest: getRequest,
+     getRequestLoading: getRequestLoading,
+     abortPostRequest: abortPostRequest,
+     abortGetRequest: abortGetRequest
+}
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,7 +1,8 @@
 // pages/list/list.js
 //获取应用实例
 const app = getApp();
-const hostUrl = require('../../config').host
+const hostUrl = require('../../config').host;
+var network  = require('../../network.js');
 
 Page({
 
@@ -9,8 +10,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list:[],
-    baseUrl: getApp().globalData.baseUrl
+    page: 1,
+    pageSize: 20,
+    hasMoreData: true,
+    contentlist: [],
+    list:[]
   },
 
   /**
@@ -33,52 +37,54 @@ Page({
   onShow: function () {
     var that = this; 
     if(app.globalData.token){
-      wx.request({
-        url: hostUrl + '/billbookdetail/listData',
-        method: 'POST',
-        data: { token: app.globalData.token },
-        header: { "token": app.globalData.token },
-        success: function (res) {
-          var list = res.data.page;
-          if (list == null) {
-            var toastText = '获取数据失败';
-            wx.showToast({
-              title: toastText,
-              icon: '',
-              duration: 2000
-            });
-          } else {
-            that.setData({
-              list: list
-            });
-          }
-        }
-      })
+      that.getDataListInfo('正在加载数据...')
     }else{
       app.userTokenReadyCallback = parm => {
-        wx.request({
-          url: hostUrl + '/billbookdetail/listData',
-          method: 'POST',
-          data: { token: app.globalData.token },
-          header: { "token": app.globalData.token },
-          success: function (res) {
-            var list = res.data.page;
-            if (list == null) {
-              var toastText = '获取数据失败';
-              wx.showToast({
-                title: toastText,
-                icon: '',
-                duration: 2000
-              });
-            } else {
-              that.setData({
-                list: list
-              });
-            }
-          }
-        })
+        that.getDataListInfo('正在加载数据...')
       }
     }
+  },
+
+  //列表数据封装代码
+   getDataListInfo: function (message) {
+    var that = this
+    var data = {
+      page: that.data.page,
+      pageSize: that.data.pageSize,
+    };
+    var data_url = hostUrl + '/billbookdetail/listData';
+    network.postRequestLoading(data_url, data, message, function (res) {
+      console.log(res)
+      var contentlistTem = that.data.contentlist
+      if (res.code == 200) {
+        if (that.data.page == 1) {
+          contentlistTem = []
+        }
+        var contentlist = res.page.list
+        if (contentlist.length < that.data.pageSize) {
+          that.setData({
+            contentlist: contentlistTem.concat(contentlist),
+            hasMoreData: false
+          })
+        } else {
+          that.setData({
+            contentlist: contentlistTem.concat(contentlist),
+            hasMoreData: true,
+            page: that.data.page + 1
+          })
+        }
+      } else {
+        wx.showToast({
+          title: res.msg,
+        })
+      }
+
+    }, function (res) {
+      wx.showToast({
+        title: '加载数据失败',
+      })
+
+    })
   },
 
   /**
@@ -106,7 +112,13 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+     if (this.data.hasMoreData) {
+      this.getDataListInfo('正在加载数据...')
+    } else {
+      wx.showToast({
+        title: '没有更多数据',
+      })
+    }
   },
 
   /**
@@ -114,6 +126,30 @@ Page({
    */
   onShareAppMessage: function () {
   },
+
+  bindTouchStart: function(e) {
+    this.startTime = e.timeStamp;
+  },
+  bindTouchEnd: function(e) {
+      this.endTime = e.timeStamp;
+  },
+  bindTap: function(e) {
+      if(this.endTime  - this.startTime < 350) {
+          console.log("点击")
+          wx.navigateTo({
+          url: '../operation/operation?id='+e.target.dataset.id
+        })
+
+      }
+  },
+
+  bingLongTap:function(e){
+    var that = this;
+    console.log("长按")
+    that.deleteData(e);
+
+  },
+
   addRow: function(){
     wx.navigateTo({
       url: '../operation/operation'
